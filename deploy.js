c8bb76ee28eb3b3a6d5152a231dc5b9ef0c79913
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 11:09:45 +0800

    Implement deploy contract

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-const { accountMneumonic, infuraApiKey } from './settings.json';
+const { accountMneumonic, infuraApiKey } = require('./settings.json');
 
 const network = 'rinkeby';
 const INFURA_API_URI = `https://${network}.infura.io/${infuraApiKey}`
@@ -13,3 +13,19 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log(`Fetched accounts: ${accounts}`);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello world!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    result.setProvider(provider); 
+
+    console.log(`Contract address is ${result.options.address}`);
+};
+
+deploy();
