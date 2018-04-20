03c54e6e0e04b9b5c5bb0a735f269e081365b1f8
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 09:22:35 +0800

    Writes first empty test to fetch the users

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+
+beforeEach(async () => {
+    // get test accounts
+    const fetchedAccounts = await web3.eth.getAccounts();
+    console.log(fetchedAccounts);
+});
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
+    });
+})
