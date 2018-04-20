7698f371488f844995a85d72b41471e5abed16d4
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 09:48:45 +0800

    Sets provider for the deployed inbox contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,8 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -17,10 +18,12 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments:[ 'Hello world!' ] })
         .send({ from: fetchedAccounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-       console.log(inbox); 
+        assert.ok(inbox.options.address);
     });
 })
