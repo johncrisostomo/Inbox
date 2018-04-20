32cf6c4ea538874634f8db077d634ea79c6d7310
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 09:56:24 +0800

    Add test to check if message can be retrieved

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,20 +10,27 @@ const { interface, bytecode } = require('../compile');
 let fetchedAccounts;
 let inbox;
 
+const INITIAL_STRING = 'Hello world!';
+
 beforeEach(async () => {
-    // get test accounts
     fetchedAccounts = await web3.eth.getAccounts();
 
-    // deploy our contract to the test network
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments:[ 'Hello world!' ] })
+        .deploy({ data: bytecode, arguments:[ INITIAL_STRING ] })
         .send({ from: fetchedAccounts[0], gas: '1000000' });
 
     inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
+
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
     });
-})
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.getMessage().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+});
