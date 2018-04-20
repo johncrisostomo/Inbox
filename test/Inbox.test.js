bd39d5dfddd08390c54365e70b5b3fa135becb82
John Crisostomo <johncrisostomo@protonmail.ch>
2018-04-20 09:30:41 +0800

    Deploys contract to Inbox

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,14 +4,23 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
+const { interface, bytecode } = require('../compile');
+
+let fetchedAccounts;
+let inbox;
+
 beforeEach(async () => {
     // get test accounts
-    const fetchedAccounts = await web3.eth.getAccounts();
-    console.log(fetchedAccounts);
+    fetchedAccounts = await web3.eth.getAccounts();
+
+    // deploy our contract to the test network
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments:[ 'Hello world!' ] })
+        .send({ from: fetchedAccounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+       console.log(inbox); 
     });
 })
